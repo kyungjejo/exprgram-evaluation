96360465d160ccb19476e5c661df7d03d41bc1ac
kyungjejo <kyungjejo@gmail.com>
2018-09-07 14:23:48 +0900

    coffee time version

--- src/Expression.jsx
+++ src/Expression.jsx
@@ -73,7 +73,7 @@ export default class Expression extends Component {
           <div>
             <div className="resources">
               <div className="title">
-                <Header as="h1">{title} ({count}/11)</Header>
+                <Header as="h1">{title} ({count}/expressions.length)</Header>
               </div>
               <div className="expression-set">
                 <div className="expression-target">
@@ -204,7 +204,7 @@ export default class Expression extends Component {
         })
         .then((response) => response.json())
         .then(res => console.log(res))
-        if (count<9)
+        if (count<expressions.length)
           this.setState({
             similarity: 0,
             appropriateness: 0,
